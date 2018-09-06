ff0a788fe1a778ce2e9d28eaddf8aace344d6ad3
Kevin Duarte <duartekevin91@gmail.com>
2018-09-06 12:29:10 -0400

    setting up api

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const path = require("path");
+const morgan = require("morgan")
+const router = require("./routes")
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.resolve(__dirname, "../public")));
+
+app.use(morgan("dev"));
+
+app.get('/api', router);
---
+++ server/routes/api1.js
@@ -0,0 +1,7 @@
+const router = require("express").Router
+
+// router.get('/[INSERT NAME]', (req, res, next) =>{
+
+// })
+
+module.exports = router
