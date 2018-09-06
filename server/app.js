98d3603b733f59213613c178881b776b8780b02e
Kevin Duarte <duartekevin91@gmail.com>
2018-09-06 12:17:41 -0400

    Set up Express server

--- server/app.js
+++ server/app.js
@@ -3,4 +3,6 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, "../public")));
